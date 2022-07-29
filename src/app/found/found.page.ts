c0d46f75a0d2f2b02e61a850c17b6974dcdf5259
ibtissemdev <ibtissem.khiri@gmail.com>
2022-07-29 10:29:12 +0200

    formfound securiation ok

--- src/app/found/found.page.ts
+++ src/app/found/found.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../api/user.service';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-found',
@@ -14,28 +15,65 @@ export class FoundPage implements OnInit {
   firstname:string;
   lastname:string;
   email:string;
-  constructor(public apiService: UserService) { }
+  ionicForm: FormGroup;
+  isSubmitted = false;
+
+
+  constructor(public apiService: UserService, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      description: ['', [Validators.required, Validators.minLength(2)]],
+      date: [null,Validators.required],
+      location: ['', [Validators.required, Validators.minLength(2)]],
+      firstname:['', [Validators.required, Validators.minLength(2)]],
+      lastname:['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+   })
+
   }
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
+
+//   getDate(e) {
+//     let date = new Date(e.target.value).toISOString().substring(0, 10);
+//     this.ionicForm.get('dob').setValue(date, {
+//        onlyself: true
+//     })
+//  }
+
 
   submitForm() {
+    //  console.log(this.ionicForm.value)
+    // let data = {
+    //   description:this.description,
+    //   status:1,
+    //   date:this.date,
+    //   location:this.location,
+    //   firstname:this.firstname,
+    //   lastname:this.lastname,
+    //   email: this.email
 
-    let data = {
-      description:this.description,
-      status:1,
-      date:this.date,
-      location:this.location,
-      firstname:this.firstname,
-      lastname:this.lastname,
-      email: this.email
+    // }
+    // console.log(data)
 
+    // this.apiService.submitForm(data).subscribe((res) => {
+    //   console.log("SUCCES ===", res);
+    // })
+    this.isSubmitted = true;
+
+    if (!this.ionicForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log(this.ionicForm.value)
+      this.apiService.submitForm(this.ionicForm.value).subscribe((res) => {
+       console.log("SUCCES ===", res);})
+       this.isSubmitted = false;
     }
-    console.log(data)
-
-    this.apiService.submitForm(data).subscribe((res) => {
-      console.log("SUCCES ===", res);
-    })
   }
+  
 
-}
+  }

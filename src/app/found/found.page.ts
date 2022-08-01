3e08dc89bdbb3de35de709cfa65f6da71c7863b9
ibtissemdev <ibtissem.khiri@gmail.com>
2022-08-01 10:09:58 +0200

    tost pour la validation du formulaire ok

--- src/app/found/found.page.ts
+++ src/app/found/found.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../api/user.service';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-found',
@@ -8,60 +10,36 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
   styleUrls: ['./found.page.scss'],
 })
 export class FoundPage implements OnInit {
-  description:string;
-  status:number;
-  date:Date;
-  location:string;
-  firstname:string;
-  lastname:string;
-  email:string;
+  description: string;
+  status: number;
+  date: Date;
+  location: string;
+  firstname: string;
+  lastname: string;
+  email: string;
   ionicForm: FormGroup;
   isSubmitted = false;
 
 
-  constructor(public apiService: UserService, public formBuilder: FormBuilder) { }
+  constructor(public apiService: UserService, public formBuilder: FormBuilder, public toastController: ToastController) { }
 
   ngOnInit() {
     this.ionicForm = this.formBuilder.group({
       description: ['', [Validators.required, Validators.minLength(2)]],
-      date: [null,Validators.required],
+      date: [null, Validators.required],
       location: ['', [Validators.required, Validators.minLength(2)]],
-      firstname:['', [Validators.required, Validators.minLength(2)]],
-      lastname:['', [Validators.required, Validators.minLength(2)]],
+      firstname: ['', [Validators.required, Validators.minLength(2)]],
+      lastname: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
-   })
+    })
 
   }
   get errorControl() {
     return this.ionicForm.controls;
   }
 
-
-//   getDate(e) {
-//     let date = new Date(e.target.value).toISOString().substring(0, 10);
-//     this.ionicForm.get('dob').setValue(date, {
-//        onlyself: true
-//     })
-//  }
-
-
   submitForm() {
-    //  console.log(this.ionicForm.value)
-    // let data = {
-    //   description:this.description,
-    //   status:1,
-    //   date:this.date,
-    //   location:this.location,
-    //   firstname:this.firstname,
-    //   lastname:this.lastname,
-    //   email: this.email
-
-    // }
-    // console.log(data)
-
-    // this.apiService.submitForm(data).subscribe((res) => {
-    //   console.log("SUCCES ===", res);
-    // })
+   
     this.isSubmitted = true;
 
     if (!this.ionicForm.valid) {
@@ -70,12 +48,31 @@ export class FoundPage implements OnInit {
     } else {
       console.log(this.ionicForm.value)
       this.apiService.submitForm(this.ionicForm.value).subscribe((res) => {
-       console.log("SUCCES ===", res);})
-       this.isSubmitted = false;
+        this.valider();
+        console.log("SUCCES ===", res);
+      })
+
+      this.isSubmitted = false;
     }
+
     this.ionicForm.reset();
   }
 
-  
+  async valider() {
+    let toast = await this.toastController.create({
 
+      message: 'Objet ajouté dans la liste des objets trouvés',
+      color: 'success',
+      duration: 3000,
+      position: 'middle',
+      buttons: [{
+        role: "cancel",
+        icon: 'close'
+
+      }]
+
+    });
+    toast.present();
   }
+
+}
